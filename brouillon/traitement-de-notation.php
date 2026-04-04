b59fb5728a2786aca5424c487c80e1064a369f43
sankavi-k <sankavi.kumanan@gmail.com>
2026-04-04 21:59:37 +0200

    Update traitement-de-notation.php

--- brouillon/traitement-de-notation.php
+++ brouillon/traitement-de-notation.php
@@ -1,1 +1,34 @@
+<?php
 
+if (!isset($_POST["commande_id"]) ||!isset($_POST["client_email"]) ||!isset($_POST["note_livraison"]) || !isset($_POST["note_produits"])
+) {
+    die("Il n'y a pas toutes les informations nécessaires.");
+}
+
+$commandeId = $_POST["commande_id"];
+$clientEmail = $_POST["client_email"];
+$noteLivraison = $_POST["note_livraison"];
+$noteProduits = $_POST["note_produits"];
+
+$contenu = file_get_contents("note.json");
+$donnees = json_decode($contenu, true);
+
+if (!isset($donnees["notes"])) {
+    $donnees["notes"] = array();
+}
+
+$Note = array(
+    "commande_id" => $commandeId,
+    "client_email" => $clientEmail,
+    "note_livraison" => $noteLivraison,
+    "note_produits" => $noteProduits
+);
+
+$donnees["notes"][] = $Note;
+
+file_put_contents("note.json", json_encode($donnees, JSON_PRETTY_PRINT));
+
+header("Location: merci.html");
+exit;
+
+?>

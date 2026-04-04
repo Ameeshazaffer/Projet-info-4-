47b340b96627ff9b4dee93ce56fc3d287e792cbd
sankavi-k <sankavi.kumanan@gmail.com>
2026-04-04 17:50:41 +0200

    Update administrateur.php

--- administrateur.php
+++ administrateur.php
@@ -1,4 +1,15 @@
 <?php
+
 $contenu = file_get_contents("utilisateurs.json");
 $donnees = json_decode($contenu, true);
+
+$utilisateurs = array();
+if (isset($donnees["utilisateurs"])) {
+  $utilisateurs = $donnees["utilisateurs"];
+}
+
+$roleapresfiltrage = "tous";
+if (isset($_GET["role"]) && $_GET["role"] != "") {
+  $roleapresfiltrage = $_GET["role"];
+}
 ?>

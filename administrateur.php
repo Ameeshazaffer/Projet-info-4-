8f9dc63033d9a3857bea6576a9a3df2276d6bec0
sankavi-k <sankavi.kumanan@gmail.com>
2026-04-04 18:02:37 +0200

    Update administrateur.php

--- administrateur.php
+++ administrateur.php
@@ -12,9 +12,10 @@ $roleapresfiltrage = "tous"; // le select est mis directement en tous
 if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer par le select dans la barre de recherche ( existe et non vide)
     $roleapresfiltrage = $_GET["role"]; // on renomme la valeur prise dans la barre par roleapresfiltrage
 }
-  
+
 ?>
-  
+
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -24,7 +25,7 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
+    
 <nav id="barre-navigation">
     <div class="conteneur-nav">
         <div class="logo-nav">
@@ -40,11 +41,10 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
 <div class="att">
     <h1>ESPACE ADMINISTRATEUR</h1>
     <h2>Gestion des utilisateurs du site</h2>
-
     <div class="select-container">
         <form method="GET" action="administrateur.php">
             <select name="role" class="select-box"  >
-                <option value="tous" <?php if ($roleapresfiltrage == "tous") echo "selected"; ?>>Tous</option>
+                <option value="tous" <?php if ($roleapresfiltrage == "tous") echo "selected"; ?>>Tous</option> <!-- permet donc de prendre le role apres le filtrage et comparer avec celui de la selection et donc si oui, le marque comme celui selectionner  -->
                 <option value="client" <?php if ($roleapresfiltrage == "client") echo "selected"; ?>>Clients</option>
                 <option value="admin" <?php if ($roleapresfiltrage == "admin") echo "selected"; ?>>Administrateurs</option>
                 <option value="livreur" <?php if ($roleapresfiltrage == "livreur") echo "selected"; ?>>Livreurs</option>
@@ -52,22 +52,60 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
             </select>
             <button type="submit">Filtrer</button>
         </form>
+    </div>
 
-    </div>
+
     <div class="liv">
-      <table>
-        <thead>
-          <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Rôle</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-</tbody>
-       </table>
-     </div>
-    </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Rôle</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+<tbody>
+    
+<?php // partie permettant de faire fonctionner le tableau avec les utilisateurs
+    $utilisateur = false; // aucune personne affichée encore
+    foreach ($utilisateurs as $index => $user) { // permet de parcourir le tableau des utilisateurs avce l'indice ( ex : indice 0 est le premier utilisateur )
+        
+        if (isset($user["role"]) && $user["role"] != "") { // définit le rôle de l'utilisateur que l'on parcourt dans cette boucle
+            $role = $user["role"];
+        }
+                                                
+        if ($roleapresfiltrage != "tous" && $role != $roleapresfiltrage) { // on prend seulement les utilisateur qui on le même role que le rôle après le filtrage et donc si pas le même on passe à l'utilisateur suivant
+            continue;
+        }
+        $utilisateur = true; // un utilisateur existe
+
+
+?>
+<tr> <!--  affiche dans le tableau les données donc nom, prenom et le rôle  -->
+    <td><?php echo htmlspecialchars($user["nom"]); ?></td>
+    <td><?php echo htmlspecialchars($user["prenom"]); ?></td>
+    <td><?php echo htmlspecialchars($role); ?></td>
+    <td>
+        <a href="profil-admin.php?id=<?php echo $index; ?>">Voir le profil</a> <!--pour l'option voir le profil permet d'aller sur la page en question avec le numero d'index pour retrouver l'utilisateur
+ -->
+    </td>
+</tr>
+    }
+<?php
+    if (!$utilisateur) { // si l'utilisateur n'est pas égal à true ça montre que y en a pas
+?>
+    <tr>
+        <td colspan="4">Il n'ya pas d'utilisateur pour le rôle selectionné.</td>
+    </tr>
+
+    }
+                    
+    </tbody>
+    </table>
+</div>
+</div>
+    
 <footer>
     <div class="logo-pied-page">
         <div class="texte-logo-pied-page">✧ÉVEIL✧</div>
@@ -94,6 +132,9 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
 
 </body>
 </html>
+
+
+
 
 
 

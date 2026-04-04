d822435a6515f99b612a8ef0bcfe713546776329
sankavi-k <sankavi.kumanan@gmail.com>
2026-04-04 18:05:06 +0200

    Update administrateur.php

--- administrateur.php
+++ administrateur.php
@@ -1,21 +1,20 @@
 <?php
+$contenu = file_get_contents("utilisateurs.json"); // il lit le fichier utilisateurs.json en entier 
+$donnees = json_decode($contenu, true); // tranforme le langage json en php sous forme de tableau 
 
-$contenu = file_get_contents("utilisateurs.json"); // il lit le fichier utilisateurs.json en entier
-$donnees = json_decode($contenu, true); // tranforme le langage json en php sous forme de tableau
-
-$utilisateurs = array();// on cré un tableau vide pour stocker les utilisateurs dedans
+$utilisateurs = array();// on cré un tableau vide pour stocker les utilisateurs dedans 
 if (isset($donnees["utilisateurs"])) { // on regarde si les utilisateurs sont bien dans le tableau
     $utilisateurs = $donnees["utilisateurs"]; // on renomme le tableau en $utilisateurs
 }
 
-$roleapresfiltrage = "tous"; // le select est mis directement en tous
+
+$roleapresfiltrage = "tous"; // le select est mis directement en tous 
 if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer par le select dans la barre de recherche ( existe et non vide)
     $roleapresfiltrage = $_GET["role"]; // on renomme la valeur prise dans la barre par roleapresfiltrage
 }
 
+
 ?>
-
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -25,7 +24,7 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    
+
 <nav id="barre-navigation">
     <div class="conteneur-nav">
         <div class="logo-nav">
@@ -41,6 +40,7 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
 <div class="att">
     <h1>ESPACE ADMINISTRATEUR</h1>
     <h2>Gestion des utilisateurs du site</h2>
+
     <div class="select-container">
         <form method="GET" action="administrateur.php">
             <select name="role" class="select-box"  >
@@ -54,58 +54,61 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
         </form>
     </div>
 
-
-    <div class="liv">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>Prénom</th>
-                    <th>Rôle</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-<tbody>
-    
-<?php // partie permettant de faire fonctionner le tableau avec les utilisateurs
-    $utilisateur = false; // aucune personne affichée encore
-    foreach ($utilisateurs as $index => $user) { // permet de parcourir le tableau des utilisateurs avce l'indice ( ex : indice 0 est le premier utilisateur )
-        
-        if (isset($user["role"]) && $user["role"] != "") { // définit le rôle de l'utilisateur que l'on parcourt dans cette boucle
-            $role = $user["role"];
-        }
-                                                
-        if ($roleapresfiltrage != "tous" && $role != $roleapresfiltrage) { // on prend seulement les utilisateur qui on le même role que le rôle après le filtrage et donc si pas le même on passe à l'utilisateur suivant
-            continue;
-        }
-        $utilisateur = true; // un utilisateur existe
+          <div class="liv">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Rôle</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php // partie permettant de faire fonctionner le tableau avec les utilisateurs 
+                    $utilisateur = false; // aucune personne affichée encore 
 
 
-?>
-<tr> <!--  affiche dans le tableau les données donc nom, prenom et le rôle  -->
-    <td><?php echo htmlspecialchars($user["nom"]); ?></td>
-    <td><?php echo htmlspecialchars($user["prenom"]); ?></td>
-    <td><?php echo htmlspecialchars($role); ?></td>
-    <td>
-        <a href="profil-admin.php?id=<?php echo $index; ?>">Voir le profil</a> <!--pour l'option voir le profil permet d'aller sur la page en question avec le numero d'index pour retrouver l'utilisateur
+                    foreach ($utilisateurs as $index => $user) { // permet de parcourir le tableau des utilisateurs avce l'indice ( ex : indice 0 est le premier utilisateur )
+                        if (isset($user["role"]) && $user["role"] != "") { // définit le rôle de l'utilisateur que l'on parcourt dans cette boucle 
+                            $role = $user["role"];
+                        }
+
+
+                        if ($roleapresfiltrage != "tous" && $role != $roleapresfiltrage) { // on prend seulement les utilisateur qui on le même role que le rôle après le filtrage et donc si pas le même on passe à l'utilisateur suivant 
+                            continue;
+                        }
+
+
+                        $utilisateur = true; // un utilisateur existe
+
+                        ?>
+                        <tr> <!--  affiche dans le tableau les données donc nom, prenom et le rôle  -->
+                            <td><?php echo htmlspecialchars($user["nom"]); ?></td>
+                            <td><?php echo htmlspecialchars($user["prenom"]); ?></td>
+                            <td><?php echo htmlspecialchars($role); ?></td>
+                            <td>
+                               <a href="profil-admin.php?id=<?php echo $index; ?>">Voir le profil</a> <!--pour l'option voir le profil permet d'aller sur la page en question avec le numero d'index pour retrouver l'utilisateur 
  -->
-    </td>
-</tr>
-    }
-<?php
-    if (!$utilisateur) { // si l'utilisateur n'est pas égal à true ça montre que y en a pas
-?>
-    <tr>
-        <td colspan="4">Il n'ya pas d'utilisateur pour le rôle selectionné.</td>
-    </tr>
 
-    }
-                    
-    </tbody>
-    </table>
-</div>
-</div>
-    
+                            </td>
+                        </tr>
+                        <?php
+                    }
+
+
+                    if (!$utilisateur) { // si l'utilisateur n'est pas égal à true ça montre que y en a pas 
+                        ?>
+                        <tr>
+                            <td colspan="4">Il n'ya pas d'utilisateur pour le rôle selectionné.</td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 <footer>
     <div class="logo-pied-page">
         <div class="texte-logo-pied-page">✧ÉVEIL✧</div>
@@ -129,11 +132,8 @@ if (isset($_GET["role"]) && $_GET["role"] != "") { // on prend la valeur envoyer
     <p style="margin-top:2rem;color:#C9B896;">© 2026 EVEIL Paris. Tous droits réservés.</p>
 </footer>
 
-
 </body>
 </html>
-
-
 
 
 
